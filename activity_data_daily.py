19ec05bc6c22f15631f97199f0a1b9d7d8922c9c
kumargovind2714 <35889970+kumargovind2714@users.noreply.github.com>
2018-01-28 22:24:29 +0530

    Add files via upload

--- activity_data_daily.py
+++ activity_data_daily.py
@@ -24,6 +24,10 @@ Program dependencies:
 Log File
 1. log_file.txt : has been set at the DEBUG level to log all activities for the run time.
 
+Output File
+1. Based on the number of activities, respective files are written in the output directory
+2. The output directory can be configured on excel_file_config.ini file
+
 '''
 
 import openpyxl
@@ -53,12 +57,21 @@ def remove_items_list(listVar,removeListVar):
         del listVar[i]
     return listVar
 
+#---------------------------------------------------------------------------
+# This function contains the business logic to read the daily activity sheet
+#----------------------------------------------------------------------------
 def getSheetResult(wbe, sheet_name):
     # get the max count of rows and cols
     m_row = sheet_name.max_row
     m_col = sheet_name.max_column
     m_row = m_row + 1
     #print(m_row, m_col)
+
+    # -- This section is to store the data of the active sheet in to List of Lists
+    # -- result_data is the list of list containing : rows and columns of the active sheet
+    # -- The reading of row starts at Row# 7 and 6 columns are read starting from column # 2
+    # -- As it is being read, the dates are converted to MMDDYYYY format and
+    # -- the result is stored in result_data as list of lists
 
     # initialize list
     result_data = []
@@ -75,6 +88,11 @@ def getSheetResult(wbe, sheet_name):
                     row_data.append(data.value)
         result_data.append(row_data)
 
+    # -- This section is to create a list - popping_Var which contains the row index of result_data
+    # -- that needed to be removed as it contains non-date values
+    # -- We are reading the col.#2 and checking if the string value is greater than 10
+    # -- If yes, then the index value is stored in popping_Var
+
     popping_Var = []
     LC_pop_len = len(result_data)
 
@@ -87,26 +105,27 @@ def getSheetResult(wbe, sheet_name):
                     popping_Var.append(i)
                 break
 
-    # Call the function to remove the list containing None or null values
+    # Call the function to remove the list of values in result_data which are referenced in popping_Var
     result_data = remove_items_list(result_data, popping_Var)
-    #print(result_data)
 
 ### -------------------------------------
 ### Remove the list of result Data which has None or null values
+### If the planned to date column has null values, those rows in the result_data list are removed.
 ### --------------------------------------
     popping_Var_None = []
     LC_pop_len_none = len(result_data)
     ## Now accessing the list of list result_data with None Value
     for i in range(0, LC_pop_len_none, 1):
         LC_data = result_data[i][5]
-        if LC_data == None:
-            popping_Var_None.append(i)
+        if LC_data == None: # checking if the list index[5] in result_data is None
+            popping_Var_None.append(i) # store the index value in popping_Var_None list
 
-    # Call the function to remove the list containing None or null values
+    # Call the function to remove the list containing None or null values as referenced in popping_Var_None
     result_data=remove_items_list(result_data,popping_Var_None)
 
-    print(result_data)
+    # return the final list to the calling function
     return result_data
+
 ### ---------End of Functions -----
 
 # import excel_file_config_readyer.py to get all its functions
@@ -134,13 +153,18 @@ wb = openpyxl.load_workbook(L_FileName,data_only='True')
 asheets = []
 result_data_sheet = []
 
-asheets = efcr.getActivitySheets()
-len_asheets = len((asheets))
+#----------------------------------------------------------------------------------------------
+# -- This section is to collect all the data from various activity sheet
+# -- Call the excel writing.py program to write the file into a csv format
+# -- The information about sheetname, total sheets etc are derived from excel_file_config.ini
+# -- The output file is determined by the name of the sheet.csv
+#----------------------------------------------------------------------------------------------
 
+asheets = efcr.getActivitySheets() # get the list of activity sheets from excel_file_config.ini
+len_asheets = len((asheets))
 for i in range(0,len_asheets,2):
     sheet_val = asheets[i]
     sheet = efcr.shName(sheet_val) # get the sheet name
-    #print(sheet)
     Asheet = wb[sheet] # creating an instance of the sheet
     result_data_sheet = getSheetResult(wb, Asheet) # getting the values of the sheet in
     outDir = efcr.outputDirectory() # getting the directory for writing the output file
